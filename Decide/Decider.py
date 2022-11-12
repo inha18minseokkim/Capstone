a32e58e0d6018147306e739c28741b5bd1ccfff8
bjm7701 <bjm7701@naver.com>
2022-11-12 20:38:10 +0900

    refactor : add new argument on abstract class

--- Decide/Decider.py
+++ Decide/Decider.py
@@ -3,7 +3,7 @@ from abc import *
 class DecisionMaker:
     #클래스의 기능 : 종목 코드를 받아서 종목들의
     @abstractmethod
-    def __init__(self):
+    def __init__(self,code,period):
         pass
     @abstractmethod
     def process(self):
