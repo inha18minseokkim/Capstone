a5426b4b3c4ad877236339ee462b3ecb6475bddd
bjm7701 <bjm7701@naver.com>
2022-11-29 20:15:11 +0900

    feat : BlackLittermanWithRegressor.py BlackLittermanStrategy 상속해서 오버라이딩 완료

--- Decide/Strategies/BlackLittermanWithRegressor.py
+++ Decide/Strategies/BlackLittermanWithRegressor.py
@@ -3,15 +3,15 @@ from numpy.linalg import inv
 import pandas as pd
 import DataCollect
 import DataRefiner
-import Decide.Decider
+import Decide
+from Decide.Strategies.BlackLitterman import BlackLittermanStrategy
 
-class BlackLittermanStrategyWithRegressor(Decide.Strategies.BlackLitterman.BlackLittermanStrategy):
+class BlackLittermanStrategyWithRegressor(BlackLittermanStrategy):
     def calc_QP(self):
-        pass
-    def dataprepro(self):
-        pass
-
+        Q = np.array([0.07669642, 0.09424969, 0.18199817, 0.12835284, -0.02027411, 0.13328767, 0.10802241])
+        P = np.eye(len(self.code))
+        return [Q,P]
 if __name__ == "__main__":
-    blsr = BlackLittermanStrategyWithRegressor( ['055550','003550','009200','000990','031440','005930','105560','042700'], 60)
+    blsr = BlackLittermanStrategyWithRegressor(  ['055550','003550','009200','000990','031440','009150','024110'], 60)
     print(blsr.w_mkt)
     print(blsr.process())
