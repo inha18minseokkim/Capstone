9a0ab5d135374e3b2fdf61f5ad5f0c6e02471ca3
bjm7701 <bjm7701@naver.com>
2022-12-02 23:00:15 +0900

    feat : FinanceCrawler.FinanceCrawl 클래스에 getFinanceInfo 함수 추가

--- FinanceCrawler.py
+++ FinanceCrawler.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 class FinanceCrawl:
     def __init__(self):
-        pass
+        self.columns = "M111000,M113000,M122700,M221100,M312000,M314000,M382100".split(",")
     def convertToStr(self,code : list):
         res = ""
         for i in code:
@@ -35,6 +35,33 @@ class FinanceCrawl:
         df = df.set_index([pd.to_datetime(df['DATE'].apply(lambda x : x + '01')),df['CODE']])
         df.drop(['DATE', 'YYMM', 'FS_YEAR', 'FS_MONTH', 'FS_QTR', 'MAIN_TERM'], axis=1, inplace=True)
         return df
+    def getFinanceInfo(self,code : str):
+        headers = {
+            "key": "B8DE27145B59CF9845FE",
+            "format": "json",
+            "success": "true",
+            "errcd": "",
+            "errmsg": "",
+            "code": "A" + code,
+            "item": "M111000,M113000,M122700,M221100,M312000,M314000,M382100",
+            "consolgb": "M",
+            "annualgb": "QQ",
+            "accdategb": "C",
+            "fraccyear": "201203",
+            "toaccyear": "202209"
+        }
+        url = "https://www.fnspace.com/Api/FinanceApi"
+        res = requests.get(url, headers)
+        resdict = res.json()
+        resarr = []
+        for ele in resdict['dataset']:
+            for d in ele['DATA']:
+                resarr.append(d)
+        df = pd.DataFrame(resarr)
+        df = df.set_index(pd.to_datetime(df['DATE'].apply(lambda x : x + '01')))
+        df.drop(['DATE', 'YYMM', 'FS_YEAR', 'FS_MONTH', 'FS_QTR', 'MAIN_TERM'], axis=1, inplace=True)
+        return df
 if __name__ == "__main__":
     fc = FinanceCrawl()
-    print(fc.getFinanceFrame(['055550','003550','009200','000990','031440','009150','024110']))
+    #print(fc.getFinanceFrame(['055550','003550','009200','000990','031440','009150','024110']))
+    print(fc.getFinanceInfo('005930'))
